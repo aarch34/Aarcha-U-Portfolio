d60e514fdccf003af27714ab1bb739aeaf66ce20
aarch34 <aarcha.u@yahoo.com>
2024-07-28 18:53:28 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `These are my experiences:<br>
+  Software Engineering Fellow, Headstarter AI:As a Software Engineering Fellow at Headstarter AI, I am embarking on a dynamic 7-week journey working on diverse projects across various fields. This immersive experience includes hands-on development in cutting-edge technologies, participating in engaging hackathons, and receiving tailored interview preparation to sharpen my skills.<br> 
+  Student Ambassador, Viral Fission:In my role as a Student Ambassador at Viral Fission, I organize and host blockchain gaming events to foster community engagement. I create engaging content, including reels and social media posts, to promote products and enhance brand visibility.<br>
+   `,
+  "cs-degree": `I have certificationS from META in HTML & CSS, and also Version Control.`,
+  "sofa-table": `Hello!, I'm Aarcha U, a B.E. student specializing in Artificial Intelligence & Machine Learning at Impact College of Engineering & Applied Sciences. With experience in web development, blockchain event management, and a passion for technology, I bring a creative and analytical approach to every project. Explore my portfolio to see my work in programming and design, and feel free to connect with me for collaboration!`,
+  tv: ` Check out my LinedIn profile:<a href="https://www.linkedin.com/in/aarcha-u" target="_blank">LinkedIn</a>.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my Github Profile: <a href="https://github.com/aarch34" target="_blank">Github</a>,
+  Contact me at aarcha.u@yahoo.com if you have any interesting job opportunities!`,
+  projects: `Some projects I've Done:<br>
+  I developed a user-friendly website for Bibha, The Event Company, focusing on responsive design and multimedia integration to enhance user engagement.<br>
+   Additionally, I created an e-commerce web app for a college project,showcasing a jewellery store using Django and MySQL, featuring a secure user authentication system, a dynamic product catalog, and a comprehensive admin dashboard. These projects showcase my skills in web development and my commitment to delivering functional and visually appealing digital solutions.`,
+  library: `These are my skills:<br>Programming Languages: Python, HTML5, CSS, C.
+<br>Web Development: Django, Kaboom.js, Responsive Design, Frontend Development.
+<br>Database Management: MySQL,MongoDB.
+<br>Version Control: Git, GitHub.`,
+  exit: `If you want to exit Aarcha U's portfolio, just close the tab.`,
+};
---
+++ vite.configure.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("vite");
+
+export default defineConfig({
+    base: "/Aarcha-U-Portfolio/",
+    build:{
+        minify:"terser",
+    },
+});
